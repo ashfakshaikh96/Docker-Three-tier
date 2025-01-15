e4cf1df647ae4dea104c0aa6305a6526d674ccf8
Ashfak Shaikh <ashpakshaikh9673@gmail.com>
2025-01-15 23:35:13 +0530

    Update init.sh

--- db/init.sh
+++ db/init.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-echo "Starting MySQL initialization..."
-
-# Execute the SQL file to create the database and table
-mysql < /docker-entrypoint-initdb.d/init.sql
-
-echo "MySQL initialization completed."
+echo 'Hey this is from shell'
