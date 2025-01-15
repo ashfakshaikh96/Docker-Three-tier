23d727845a00d2b09053b27e0172d16dda025b48
Ashfak Shaikh <ashpakshaikh9673@gmail.com>
2025-01-15 23:32:55 +0530

    Updated submit.php

--- app/myphpcode/submit.php
+++ app/myphpcode/submit.php
@@ -1,31 +1,27 @@
 <?php
-// Database connection settings
-$servername = "db"; // Matches the db service name in docker-compose.yml
+$name=$_POST["username"];
+$email=$_POST["email"];
+$password= $_POST["password"];
+echo $name;
+echo $email;
+echo $password;
+
+$servername = "db";
 $username = "root";
-$password = "Pass@123";
-$database = "facebook";
+$mpassword = "Pass@123";
+$dbname = "insta";
 
-// Create a connection to the database
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Retrieve form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-// Insert form data into a database table
-$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-// Close the database connection
-$conn->close();
-?>
+// Create connection
+ $conn = mysqli_connect($servername, $username, $mpassword, $dbname);
+// // Check connection
+ if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+   }
+  $sql = "INSERT INTO users (name,email,password)VALUES('$name','$email','$password')";
+  if (mysqli_query($conn, $sql)) {
+               echo "New record created successfully";
+     } else {
+       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+       }
+       mysqli_close($conn);
+       ?>
